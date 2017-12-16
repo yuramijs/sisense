3a3fa7038ac7750bb6c689a6c95584883ecaf54f
yura123 <y.dmitriev@mobilunity.com>
2017-12-16 20:03:13 +0200

    add table ui

--- src/routes/table/Table.js
+++ src/routes/table/Table.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {Component, Fragment} from 'react';
 import {sortBy} from 'lodash';
 
 import {connect} from 'react-redux';
 
 import getTable from '../../actions/getTable';
 
-class Table extends React.Component {
+class Table extends Component {
   constructor() {
     super();
     this.state = {
@@ -38,20 +38,37 @@ class Table extends React.Component {
   };
 
   render() {
-    console.log(this.props.table.getTables.table);
     return (
       <div className="card">
         <div onClick={() => this.sortByName()}>sortByName</div>
         <div onClick={() => this.getTable()}>getTable</div>
         <p>Search</p>
         <input type="text" onChange={this.onChange.bind(this)} />
-        Table
-        <div>
-          {this.props.table.getTables.table && this.props.table.getTables.table.map((item, index) => (
-              <div key={index}>{item.name}</div>
-          ))
-          }
-        </div>
+        <table className="table">
+          <thead>
+          <tr>
+            <th>id</th>
+            <th>Name</th>
+            <th>Date</th>
+            <th>String</th>
+            <th>Number</th>
+          </tr>
+          </thead>
+          <tbody>
+            {this.props.table.getTables.table &&
+            this.props.table.getTables.table.map((item, index) => (
+              <Fragment key={index}>
+                <tr>
+                  <td>{item._id}</td>
+                  <td>{item.name}</td>
+                  <td>Date</td>
+                  <td>String</td>
+                  <td>Number</td>
+                </tr>
+              </Fragment>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
